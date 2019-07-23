7f9597a210616cfbd36f5a14d29b9bd5d8945700
Ruwai <chrisl6uie@gmail.com>
2019-07-22 18:21:46 -0700

    queueeueue

--- queue/queue.py
+++ queue/queue.py
@@ -3,13 +3,72 @@ class Queue:
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = linkedList()
 
   def enqueue(self, item):
-    pass
-  
+    self.storage.enqueue(item)
+
   def dequeue(self):
-    pass
+    return self.storage.dequeue()
 
   def len(self):
-    pass
+    return self.storage.get_size()
+
+class Node:
+  def __init__(self, value=None, nextNode=None):
+    self.value = value
+    self.nextNode = nextNode
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.nextNode
+
+  def set_next(self, new_next):
+    self.nextNode = new_next
+
+  def set_value(self, new_value):
+    self.value = new_value
+
+class linkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+    self.size = 0
+
+  def get_size(self):
+    return self.size
+
+  def dequeue(self, node=None):
+    if self.head == None:
+      return None
+    elif self.head == self.tail:
+      last_node = self.tail
+      self.head = None
+      self.tail = None
+      self.size -= 1
+      return last_node.get_value()
+    else:
+      tmp = self.head
+      self.head = self.head.get_next()
+      self.size -= 1
+      return tmp.get_value()
+
+  def enqueue(self, item):
+    node = Node(item)
+    if self.tail == None:
+      self.tail = node
+      self.head = node
+    else:
+      self.tail.set_next(node)
+      self.tail = node
+    self.size += 1
+
+if __name__ == "__main__":
+
+  items_queue = Queue()
+  print(items_queue.len())
+  items_queue.enqueue(1)
+  items_queue.enqueue(2)
+  print(items_queue.len())
